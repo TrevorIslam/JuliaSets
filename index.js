8fac876beecc3df2a9b12f41f07ad4b5dfe360a4
TrevorIslam <trevorislam@gmail.com>
2020-05-18 09:06:28 -0700

    asdf

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var zoom = 200;
 var creal;
 var cimag;
 var c;
+var running = false;
 
 function setup () {
   console.clear();
@@ -11,13 +12,19 @@ function setup () {
   var height = 600;
   noStroke();
   createCanvas(width, height);
-  translate(width * 0.5, height * 0.5);
-  creal = 0.285;
-  cimag = 0.01;
+  creal = 0;
+  cimag = 0;
   c = new ComplexNumber(creal, cimag);
 }
 
 function draw () {
+  if (running) {
+    main();
+    running = !running;
+  }
+}
+
+function main () {
   resizeCanvas(width, height);
   translate(width * 0.5, height * 0.5);
   c = new ComplexNumber(creal, cimag);
@@ -29,51 +36,63 @@ function draw () {
       let saturation = 100;
       let brightness = (m < maxIterations) ? 100 : 0;
       stroke(hue, saturation, brightness);
-      point(i, j);
+      point(i, -j);
     }
   }
+  drawAxes();
+}
+
+function drawAxes () {
   strokeWeight(1);
   stroke(0,0,0);
   line(-0.5 * width, 0, 0.5 * width, 0);
   line(0, -0.5 * height, 0, 0.5 * height);
 
-  line(-0.5 * zoom, -10, -0.5 * zoom, 10);
+  //line(-0.5 * zoom, -10, -0.5 * zoom, 10); //-0.5
   line(-1 * zoom, -10, -1 * zoom, 10);
-  line(-1.5 * zoom, -10, -1.5 * zoom, 10);
+  //line(-1.5 * zoom, -10, -1.5 * zoom, 10);
   line(-2 * zoom, -10, -2 * zoom, 10);
-  line(0.5 * zoom, -10, 0.5 * zoom, 10);
+  //line(0.5 * zoom, -10, 0.5 * zoom, 10);
   line(1 * zoom, -10, 1 * zoom, 10);
-  line(1.5 * zoom, -10, 1.5 * zoom, 10);
+  //line(1.5 * zoom, -10, 1.5 * zoom, 10);
   line(2 * zoom, -10, 2 * zoom, 10);
 
-  line(10, -0.5 * zoom, -10, -0.5 * zoom);
+  //line(10, -0.5 * zoom, -10, -0.5 * zoom);
   line(10, -1 * zoom, -10, -1 * zoom);
-  line(10, -1.5 * zoom, -10, -1.5 * zoom);
+  //line(10, -1.5 * zoom, -10, -1.5 * zoom);
   line(10, -2 * zoom, -10, -2 * zoom);
-  line(10, 0.5 * zoom, -10, 0.5 * zoom);
+  //line(10, 0.5 * zoom, -10, 0.5 * zoom);
   line(10, 1 * zoom, -10, 1 * zoom);
-  line(10, 1.5 * zoom, -10, 1.5 * zoom);
+  //line(10, 1.5 * zoom, -10, 1.5 * zoom);
   line(10, 2 * zoom, -10, 2 * zoom);
 }
 
 $(document).ready(() => {
   attachSliderEvents();
+  attachButtonEvents();
 })
 
 function attachSliderEvents () {
-  $("#crealslider").on("input", function(){
+  $("#crealslider").on("input", function() {
     let val = parseFloat($(this).val());
     creal = val;
     $("#crealvalue").text(val);
   })
-  $("#cimagslider").on("input", function(){
+  $("#cimagslider").on("input", function() {
     let val = parseFloat($(this).val());
     cimag = val;
     $("#cimagvalue").text(val);
   })
-  $("#zoomslider").on("input", function(){
+  $("#zoomslider").on("input", function() {
     let val = parseFloat($(this).val());
     zoom = val;
     $("#zoomvalue").text(val);
+  })
+}
+
+function attachButtonEvents () {
+  $("#startbutton").click(function() {
+    running = !running;
+    console.log(running);
   })
 }
